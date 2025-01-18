27aed32a6948411d116b694af7c7ed5049063061
Zinobali <lb17349842897@163.com>
2025-01-19 02:20:10 +0800

    kuchen
    -激情在燃烧
    -启动动画

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+
+    // 激情在燃烧
+    QPixmap pixmap(":/image/kuchen.png");
+    pixmap = pixmap.scaled(300, 300, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    QSplashScreen splash(pixmap);
+    splash.show();
+
+
+    QTimer::singleShot(3000, [&](){
+        w.show();
+        splash.finish(&w);
+    });
+
     return a.exec();
 }
